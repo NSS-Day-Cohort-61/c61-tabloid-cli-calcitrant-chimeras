3691c6951d5dc19797535cad371eef1e72fef635
Will Hedges <wshedges@outlook.com>
2023-03-21 10:49:13 -0500

    all journal entries displaying * implements journal repo, manager * added options to list journal entries or return to main menu

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 using TabloidCLI.Models;
 
 namespace TabloidCLI.Repositories
@@ -13,7 +12,31 @@ namespace TabloidCLI.Repositories
 
         public List<Journal> GetAll()
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Title, Content, CreateDateTime FROM Journal";
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Journal> journals = new List<Journal>();
+                    while (reader.Read())
+                    {
+                        Journal journal = new Journal()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                        };
+                        journals.Add(journal);
+                    }
+
+                    reader.Close();
+                    return journals;
+                }
+            }
         }
 
         public Journal Get(int id) 
---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List Entries");
+            Console.WriteLine(" 2) Add Entry");
+            Console.WriteLine(" 3) Edit Entry");
+            Console.WriteLine(" 4) Delete Entry");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch(choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Journal> entries = _journalRepository.GetAll();
+            Console.WriteLine("All Journal Entries:");
+            foreach (Journal entry in entries)
+            {
+                Console.WriteLine($" {entry.Id}. {entry.Title}");
+            }
+        }
+    }
+}
