26dcaeceeec52a976554b612e82f68988406edd2
Will Hedges <wshedges@outlook.com>
2023-03-21 10:20:53 -0500

    sketched out JournalRepository * added Journal model

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    public class JournalRepository : DatabaseConnector, IRepository<Journal>
+    {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Journal Get(int id) 
+        { 
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Journal journal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Journal journal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
