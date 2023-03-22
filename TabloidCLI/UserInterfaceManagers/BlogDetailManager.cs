cbf06fdf1c1dec4ee5f2ad72fc1fc06ef7601d5a
Chris Hanson <chanson8@gmail.com>
2023-03-22 09:53:42 -0500

    done with #34 blog details

--- TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -69,6 +69,15 @@ namespace TabloidCLI.UserInterfaceManagers
             Console.WriteLine();
             Console.WriteLine($"Blog title: {blog.Title}");
             Console.WriteLine($"URL: {blog.Url}");
+            if (blog.Tags.Count > 0)
+            {
+                Console.WriteLine($"All blog tags: ");
+                foreach (Tag tag in blog.Tags)
+                {
+                    Console.WriteLine($"{tag.Name}");
+                }
+            }
+
             Console.WriteLine();
         }
 
@@ -116,7 +125,29 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void RemoveTag()
         {
+            Blog blog = _blogRepository.Get(_blogId);
 
+            Console.WriteLine($"Which tag would you like to remove from {blog.Title}?");
+            List<Tag> tags = blog.Tags;
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.WriteLine("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                Tag tag = tags[choice - 1];
+                _blogRepository.DeleteTag(blog.Id, tag.Id);
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("Invalid Selection. Won't remove any tags.");
+            }
         }
     }
 }
